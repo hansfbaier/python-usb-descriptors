ef67ccf814d0a976cafa2a6bf7d33930b7e2f375
Hans Baier <foss@hans-baier.de>
2026-07-25 06:29:10 +0700

    publish: align package metadata for usb-descriptors v0.2.0

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-
 from setuptools import setup, find_packages
 
 setup(
@@ -6,23 +5,23 @@ setup(
     # Vitals
     name='usb_descriptors',
     license='BSD',
-    url='https://github.com/hansfbaier/python-usb-descriptors',
+    url='https://github.com/amaranth-farm/python-usb-descriptors',
     author='Hans Baier',
-    author_email='hansfbaier@gmail.com',
+    author_email='foss@hans-baier.de',
     description='python library providing utilities, data structures, constants, parsers, and tools for working with USB data',
     use_scm_version= {
         "root": '..',
         "relative_to": __file__,
         "version_scheme": "guess-next-dev",
         "local_scheme": lambda version : version.format_choice("+{node}", "+{node}.dirty"),
-        "fallback_version": "0.1.1"
+        "fallback_version": "0.2.0"
     },
 
     # Imports / exports / requirements.
     platforms='any',
     packages=find_packages(),
     include_package_data=True,
-    python_requires="~=3.7",
+    python_requires=">=3.9",
     install_requires=['construct'],
     setup_requires=['setuptools', 'setuptools_scm', 'wheel'],
 
